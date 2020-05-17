8d2fddaebc6aba6426058f43ee289df60c4c8d0a
MelihErpek <61746050+MelihErpek@users.noreply.github.com>
2020-05-17 04:34:02 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,120 +1,118 @@
-const canvas = document.getElementById("myCanvas");
-const ctx = canvas.getContext("2d");
-let snakeX=100;
-let snakeY=100;
-let currX=0 ;
-let currY=0;
-const setup = ()=>{
-  ctx.fillStyle = "#d65d5d";
-  ctx.fillRect(0,0,500,500);
-}
-let Snake = {
-    color : "#ffffff",
-    xSpeed : 10,
-    ySpeed : 10,
-    coordX : 100,
-    coordY : 100
-
-}
-let Food = {
-  dimensionX : 10,
-  dimensionY : 10,
-  color : '#75b1c4',
-  coordX :50,
-  coordY : 10
-}
-const generateFood  = () =>
+class SnakeGame
 {
-  ctx.fillStyle = Food.color;
-  let randomX = Math.floor(Math.random()*500);
-  let randomY = Math.floor(Math.random()*500);
-  console.log(randomX,randomY);
-  ctx.fillRect(randomX,randomY,Food.dimensionX,Food.dimensionY);
-}
- 
-const moveRightSnake = () =>
-{
-    currX = snakeX + Snake.xSpeed;
-    currY = snakeY ;
-    ctx.fillStyle = Snake.color;
-    ctx.fillRect(currX,currY,10,10);
-    snakeX = currX;
-    snakeY = currY;
-}
-const moveLeftSnake = () =>
-{
-    currX = snakeX - Snake.xSpeed;
-    currY = snakeY ;
-    ctx.fillStyle = Snake.color;
-    ctx.fillRect(currX,currY,10,10);
-    snakeX = currX;
-    snakeY = currY;
-}
-const moveDownSnake = () =>
-{
-    currX = snakeX ;
-    currY = snakeY +Snake.ySpeed;
-    ctx.fillStyle = Snake.color;
-    ctx.fillRect(currX,currY,10,10);
-    snakeX = currX;
-    snakeY = currY;
-}
-const moveUpSnake = () =>
-{
-    currX = snakeX ;
-    currY = snakeY - Snake.ySpeed;
-    ctx.fillStyle = Snake.color;
-    ctx.fillRect(currX,currY,10,10);
-    snakeX = currX;
-    snakeY = currY;
-}
-const pressedKey = (key)=>
-{
-  switch(key.which)
+  constructor()
   {
-    case 37:
-      moveLeftSnake();
-      break;
-    case 38:
-      moveUpSnake();
-      break;
-    case 39:
-      moveRightSnake();
-      break;
-    case 40:
-      moveDownSnake();
-      break;
+    this.canvas = document.getElementById('myCanvas');
+    this.context = this.canvas.getContext('2d');
+    document.addEventListener('keydown',this.onKeyPress.bind(this));
+
   }
   
-}
-const gameLoop = () =>
-{
-  setup();
-  generateFood();
-  document.onkeydown = pressedKey;
+  init()
+  {
+    this.positionX = this.positionY = 10;
+    this.foodX = this.foodY = 5;
+    this.tailSize = 5;
+    this.trail = [];
+    this.gridSize = this.tileCount = 20;
+    this.velocityX = this.velocityY = 0;
+    this.timer = setInterval(this.loop.bind(this),1000/15);
+  }
+  reset()
+  {
+    clearInterval(this.timer);
+    this.init();
+  }
+  loop ()
+  {
+    this.update();
+    this.draw();
+  }
+  update()
+  {
+    this.positionX += this.velocityX;
+    this.positionY += this.velocityY;
+
+    if(this.positionX < 0)
+    {
+      this.positionX = this.tileCount -1;
+    }
+    if(this.positionY < 0)
+    {
+      this.positionY = this.tileCount -1;
+    }
+    if(this.positionX > this.tileCount-1)
+    {
+      this.positionX = 0;
+    }
+    if(this.positionY > this.tileCount-1)
+    {
+      this.positionY = 0;
+    }
+    this.trail.forEach(a => {
+      if(this.positionX == a.positionX && this.positionY == a.positionY)
+      {
+        this.reset();
+      }
+    })
+    this.trail.push({positionX: this.positionX, positionY : this.positionY});
+    console.log(this.trail);
+    while(this.trail.length > this.tailSize)
+    {
+      this.trail.shift(); 
+    }
+    if(this.foodX == this.positionX && this.foodY == this.positionY)
+    {
+      this.tailSize++;
+      this.foodX = Math.floor(Math.random()*this.tileCount);
+      this.foodY = Math.floor(Math.random()*this.tileCount);
+    }
+
+
+  }
+  draw()
+  {
+    this.context.fillStyle = "#d65d5d";
+    this.context.fillRect(0,0,400,400);
+
+    this.context.fillStyle = 'white';
+    this.context.font = '20px Arial';
+    this.context.fillText(this.tailSize,20,40);
+    
+    this.context.fillStyle = '#ffffff'
+    this.trail.forEach( a => {
+      this.context.fillRect(a.positionX * this.gridSize , a.positionY * this.gridSize,this.gridSize-5,this.gridSize-5);
+    })
+
+    this.context.fillStyle = '#75b1c4';
+    this.context.fillRect(this.foodX * this.gridSize,this.foodY*this.gridSize,this.gridSize-5,this.gridSize-5);
+
+  }
+
+  onKeyPress(key)
+  {
+    if(key.keyCode == 37 && this.velocityX != 1)
+    {
+      this.velocityX = -1;
+      this.velocityY = 0;
+    }
+    if(key.keyCode == 38 && this.velocityY != 1)
+    {
+      this.velocityX = 0;
+      this.velocityY = -1;
+    }
+    if(key.keyCode == 39 && this.velocityX != -1)
+    {
+      this.velocityX = 1;
+      this.velocityY = 0;
+    }
+    if(key.keyCode == 40 && this.velocityY != -1)
+    {
+      this.velocityX = 0;
+      this.velocityY = 1;
+    }
+  }
 }
 
-gameLoop();
-
-/*var pressedKey2;
-  pressedKey2 = key.which;*/
-  
-  /*if(pressedKey2 == 37)
-  {
-    moveLeftSnake();
-    
-  }
-  else if (pressedKey2 == 38)
-  {
-    moveUpSnake();
-    
-  }
-  else if (pressedKey2 == 39)
-  {
-    moveRightSnake();
-    
-  }
-  else if (pressedKey2 == 40)
-  {
-    moveDownSnake();
-  }*/
+const game  = new SnakeGame();
+window.onload = () => game.init();
